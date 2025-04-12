9fb8fba3640d0351acae3cba9c9c51999024418b
ArthurEstudos <121885276+ArthurEstudos@users.noreply.github.com>
2025-04-12 15:44:35 -0300

    Add files via upload

---
+++ conceitos.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("POO")
+
+#string = texto literal
+nome = "arthur placidino de souza"
+
+#inteiro
+idade = 18
+idade = 19 
+ 
+print(nome)
+print(idade)
+nomeDigitado = input("por favor digite um nome: ")
+print("o nome digitado foi: " , nomeDigitado)
+
+
---
+++ operadores.py
@@ -0,0 +1,30 @@
+n1 = 7 
+n2 = 9
+
+aprovado = True
+media = 6
+minimo = 7
+
+print("operadores arirméticos")
+print("soma: ", n1 + n2)
+print("subtração: ", n1 - n2)
+print("multiplicação: ", n1 * n2)
+print("divisão: ", n1 / n2)
+print("resto: ", n1 % n2)
+
+
+print()
+print("operadores relacionais")
+print("igualdade: ",n1 == n2)
+print("diferença: ",n1 != n2)
+print("menor que: ",n1 < n2)
+print("maior que: ",n1 > n2)
+print("menor ou igual a: ",n1 <= n2)
+print("maior ou igual a: ",n1 >= n2)
+
+print()
+print("operdores lógicos")
+print(aprovado)
+print(not aprovado)
+print(n1 >= 7 and minimo == 7)
+print(n1 > 6 or minimo == 7)
