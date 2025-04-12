b0a12eaca6af2de65216300f843b40e702888280
ArthurEstudos <121885276+ArthurEstudos@users.noreply.github.com>
2025-04-12 15:45:55 -0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,17 @@
+n1 = 10
+n2 = 4
+n3 = 6
+n4 = 2
+
+soma = n1 + n2 + n3 + n4
+media = soma / 4
+print(soma)
+print(media)
+
+if media >=7:
+    print("aprovado")   
+elif media < 5:
+        print("reprovado")
+else:
+    print("Em recuperação")
+
